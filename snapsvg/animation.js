e0d6e9612d711e65cf116d15671b7a5e3cc4e36e
ericchen0121 <eric.chen0121@gmail.com>
2014-07-27 16:47:20 -0700

    add thin descender lines

--- snapsvg/animation.js
+++ snapsvg/animation.js
@@ -6,8 +6,6 @@ window.onload = function ()
     // can create a Set of the vertical lines
     var lineSet = Snap.set();
 
-    // var text = s.text(110, 110, ["lexus hs250h", "audi a4", "mercedes benz"]);
-
     // builds "quantity" of vertical lines/rectangles, "distance" pixels apart
     var buildVerticalLines = function(x, y, width, height, quantity, distance){
      for(var i=0; i < quantity; i++){
@@ -17,33 +15,38 @@ window.onload = function ()
       }
     }
 
-    // create text separately, and set text-anchor to "end"
-    var lexusText = s.text(180, 100, ["134hp", "Lexus CT200H"])
-      // may want to style separately
-      // lexusText.select('tspan:first-of-type').attr({fontSize:'2.2em'});
-      // lexusText.select('tspan:last-of-type').attr({dx:-100, dy:20});
-    var mercedesText = s.text(380, 100, ["174hp", "Mercedes B class ED"])
-    var bmwText = s.text(220, 400, ["170hp", "BMW i3"])
-    var bmw2Text = s.text(560, 400, ["180hp","BMW 320i"])
-    var audiText = s.text(1100, 80, ["204hp", "A3 e-tron"])
+    // Text creation
+    var lexusText = s.text(220, 100, "Lexus CT200H").attr({"text-anchor":"end"})
+    var lexusHPText = s.text(220, 70, "134hp").attr({"text-anchor":"end"})
+    var mercedesText = s.text(580, 100, "Mercedes B class ED").attr({"text-anchor":"end"})
+    var mercedesHPText = s.text(580, 70, "174hp").attr({"text-anchor":"end"})
+    var bmwText = s.text(420, 400, "BMW i3").attr({"text-anchor":"end"})
+    var bmwHPText = s.text(420, 370, "170hp").attr({"text-anchor":"end"})
+    var bmw2Text = s.text(660, 400, "BMW 320i").attr({"text-anchor":"end"})
+    var bmw2HPText = s.text(660, 370, "180hp").attr({"text-anchor":"end"})
+    var audiText = s.text(1070, 80, "A3 e-tron").attr({"text-anchor":"end"})
+    var audiHPText = s.text(1070, 50, "204hp").attr({"text-anchor":"end"})
 
+    // Create a set to iterate text styles
+    var horsepowerSet = Snap.set();
+    horsepowerSet.push(lexusHPText)
+      .push(mercedesHPText)
+      .push(bmwHPText)
+      .push(bmw2HPText)
+      .push(audiHPText);
 
-    var textSet = Snap.set();
-    textSet.push(lexusText)
-      .push(mercedesText)
-      .push(bmwText)
-      .push(bmw2Text)
-      .push(audiText);
-
-    horsepowerStyleText = function(text){
-      text.select('tspan:first-of-type').attr({fontSize:'2.2em'});
-      text.select('tspan:last-of-type').attr({dx:-100, dy:20});
+    horsepowerStyle = function(text){
+      text.attr({fontSize:'2.2em'});
     };
 
-    textSet.forEach(horsepowerStyleText);
+    horsepowerSet.forEach(horsepowerStyle);
 
+    // Create lines extending from text to the vertical lines
+    // TO DO
 
-    buildVerticalLines(100, 200, 2, 150, 50, 20);
+    // Call Functions
+    buildVerticalLines(100, 145, 2, 150, 50, 20);
+    buildVerticalLines(161, 295, 1, 30, 47, 20);
 
   }
 
