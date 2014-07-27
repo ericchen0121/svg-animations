632534308b534dfa36e31a1785e22542931d9374
ericchen0121 <eric.chen0121@gmail.com>
2014-07-27 00:59:46 -0700

    add vertical line builder

--- snapsvg/animation.js
+++ snapsvg/animation.js
@@ -2,20 +2,16 @@ window.onload = function ()
 {
   var test = function(){
     var s = Snap("#svg");
-    // rect(x,y,width, height)
-    var line = s.rect(100, 100, 2, 250);
     // var text = s.text(110, 110, ["lexus hs250h", "audi a4", "mercedes benz"]);
 
-    for(var i=0; i < 50; i++){
-      s.rect(100 + (i*20), 100, 2, 250);
+    // builds "quantity" of vertical lines/rectangles, "distance" pixels apart
+    var buildVerticalLines = function(x, y, width, height, quantity, distance){
+     for(var i=0; i < quantity; i++){
+        s.rect(x + (i*distance), y, width, height);
+      }
     }
 
-  }
-
-  // how does prototype work
-  var build_lines = function(quantity){
-
-
+    buildVerticalLines(50, 100, 2, 250, 30, 20);
   }
 
   test();
