fcbd50e282a6952f1b5406ce5e021a6be7a4058c
ericchen0121 <eric.chen0121@gmail.com>
2014-07-27 02:31:43 -0700

    add vertical line fade in function

--- snapsvg/animation.js
+++ snapsvg/animation.js
@@ -2,17 +2,26 @@ window.onload = function ()
 {
   var test = function(){
     var s = Snap("#svg");
+
+    // can create a Set of the vertical lines
+    var lineSet = Snap.set();
+
     // var text = s.text(110, 110, ["lexus hs250h", "audi a4", "mercedes benz"]);
 
     // builds "quantity" of vertical lines/rectangles, "distance" pixels apart
     var buildVerticalLines = function(x, y, width, height, quantity, distance){
      for(var i=0; i < quantity; i++){
-        s.rect(x + (i*distance), y, width, height);
+        s.rect(x + (i*distance), y, width, height)
+          .attr({opacity:0})
+          .animate({opacity:1}, 3000, mina.easeout );
       }
     }
 
+
     buildVerticalLines(50, 100, 2, 250, 30, 20);
+
   }
+
 
   test();
 }
