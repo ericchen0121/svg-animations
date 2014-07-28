683724ef3989535100bbcdabc542c6c52ba026cd
ericchen0121 <eric.chen0121@gmail.com>
2014-07-27 23:46:10 -0700

    add orange gradient fill and stroke

--- snapsvg/animation_sun_power.js
+++ snapsvg/animation_sun_power.js
@@ -2,16 +2,21 @@ window.onload = function(){
 
   var s = Snap("#svg");
 
+  // g is a linear gradient, relative to the element applied
+  var g = s.paper.gradient("r(0,1,1)#cf5300-#fff")
+
   var circleAnimation = function(lines){
     var line = s.rect(500, 400, 2, 350).attr({transform:"rotate(0 501 400)"})
     var line1 = s.rect(500, 400, 2, 350).attr({transform:"rotate(45 501 400)"})
-    var line2 = s.rect(500, 400, 2, 350).attr({transform:"rotate(90 501 400)"})
-    var line3 = s.rect(500, 400, 2, 350).attr({transform:"rotate(135 501 400)"})
-    var line4 = s.rect(500, 400, 2, 350).attr({transform:"rotate(180 501 400)"})
-    var line5 = s.rect(500, 400, 2, 350).attr({transform:"rotate(225 501 400)"})
+    var line2 = s.rect(500, 400, 2, 350).attr({transform:"rotate(90 501 400)" , fill: g, stroke: g})
+    var line3 = s.rect(500, 400, 2, 350).attr({transform:"rotate(135 501 400)", fill: g, stroke: g})
+    var line4 = s.rect(500, 400, 2, 350).attr({transform:"rotate(180 501 400)", fill: g, stroke: g})
+    var line5 = s.rect(500, 400, 2, 350).attr({transform:"rotate(225 501 400)", fill: g, stroke: g})
     var line6 = s.rect(500, 400, 2, 350).attr({transform:"rotate(270 501 400)"})
     var line7 = s.rect(500, 400, 2, 350).attr({transform:"rotate(315 501 400)"})
     var lineGroup = s.group(line, line1, line2, line3, line4, line5, line6, line7)
+
+    var centerCircle = s.circle(500, 400, 160).attr({ fill: "#fff"})
   }
 
   var radialFlare = function(numberOfLines){
@@ -32,11 +37,14 @@ window.onload = function(){
   }
 
   // create the center circle
-  // var centerCircle = s.circle(500, 400, 100).attr({ fill: "#fff"})
+  // s.circle(500, 400, 100).attr({ fill: "#fff"})
+
+  // create the circle manually
+  circleAnimation();
 
    // create the radial
    // radialFlare(56);
 
-   halfRadialFlare(7);
+   // halfRadialFlare(27);
 
 }
