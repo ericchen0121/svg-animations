f5cee4b03a902daf001a84e79c1d8ddd9b5a2006
ericchen0121 <eric.chen0121@gmail.com>
2014-07-27 20:40:21 -0700

    add radial creation function

--- snapsvg/animation_sun_power.js
+++ snapsvg/animation_sun_power.js
@@ -1,8 +1,8 @@
 window.onload = function(){
 
-   var s = Snap("#svg");
+  var s = Snap("#svg");
 
-   var circleAnimation = function(lines){
+  var circleAnimation = function(lines){
     var line = s.rect(500, 400, 2, 250).attr({transform:"rotate(0 501 400)"})
     var line1 = s.rect(500, 400, 2, 250).attr({transform:"rotate(45 501 400)"})
     var line2 = s.rect(500, 400, 2, 250).attr({transform:"rotate(90 501 400)"})
@@ -12,9 +12,20 @@ window.onload = function(){
     var line6 = s.rect(500, 400, 2, 250).attr({transform:"rotate(270 501 400)"})
     var line7 = s.rect(500, 400, 2, 250).attr({transform:"rotate(315 501 400)"})
     var lineGroup = s.group(line, line1, line2, line3, line4, line5, line6, line7)
-    var centerCircle = s.circle(500, 400, 100).attr({ fill: "#fff"})
-   }
+  }
 
-   circleAnimation(50);
+  var radialFlare = function(numberOfLines){
+    for(i=0; i<numberOfLines; i++){
+      var degree = (360 / numberOfLines) * i
+      s.rect(500, 400, 2, 250).attr({transform:"rotate(" + degree + " 501 400)"})
+    }
+    var centerCircle = s.circle(500, 400, 100).attr({ fill: "#fff"})
+  }
+
+  // create the center circle
+  var centerCircle = s.circle(500, 400, 100).attr({ fill: "#fff"})
+
+   // create the radial
+   radialFlare(12);
 
 }
