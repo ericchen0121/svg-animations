9142ce47fd3b35bb4964e8eec8a871f956523c22
ericchen0121 <eric.chen0121@gmail.com>
2014-07-27 20:30:47 -0700

    add base version of radial sun lines

---
+++ snapsvg/animation_sun_power.js
@@ -0,0 +1,20 @@
+window.onload = function(){
+
+   var s = Snap("#svg");
+
+   var circleAnimation = function(lines){
+    var line = s.rect(500, 400, 2, 250).attr({transform:"rotate(0 501 400)"})
+    var line1 = s.rect(500, 400, 2, 250).attr({transform:"rotate(45 501 400)"})
+    var line2 = s.rect(500, 400, 2, 250).attr({transform:"rotate(90 501 400)"})
+    var line3 = s.rect(500, 400, 2, 250).attr({transform:"rotate(135 501 400)"})
+    var line4 = s.rect(500, 400, 2, 250).attr({transform:"rotate(180 501 400)"})
+    var line5 = s.rect(500, 400, 2, 250).attr({transform:"rotate(225 501 400)"})
+    var line6 = s.rect(500, 400, 2, 250).attr({transform:"rotate(270 501 400)"})
+    var line7 = s.rect(500, 400, 2, 250).attr({transform:"rotate(315 501 400)"})
+    var lineGroup = s.group(line, line1, line2, line3, line4, line5, line6, line7)
+    var centerCircle = s.circle(500, 400, 100).attr({ fill: "#fff"})
+   }
+
+   circleAnimation(50);
+
+}
