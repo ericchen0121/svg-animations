6a21624879f94dd17c778415228c6432a89f67b6
ericchen0121 <eric.chen0121@gmail.com>
2014-07-28 19:14:24 -0700

    make radial animation extensible

--- snapsvg/horizon.js
+++ snapsvg/horizon.js
@@ -12,7 +12,7 @@ window.onload = function(){
 
     for(i=0; i<=numberOfLines; i++){
       var degree = ((totalDegrees / numberOfLines) * i) + 90
-      var line = s.paper.rect(500, 400, 2, 350).attr({opacity:0, transform:"rotate(" + degree + " 501 400)"})
+      var line = s.paper.rect(radial.cx, radial.cy, 2, radial.r).attr({opacity:0, transform:"rotate(" + degree + " " + (radial.cx + 1) + " " + radial.cy + ")"})
 
       // color the first half of the radial lines
       if (i < (numberOfLines / 2)){
@@ -24,7 +24,7 @@ window.onload = function(){
       fadeInDelay(line, i);
     }
 
-    var centerMask = s.paper.circle(500, 400, 160).attr({ fill: "#fff"})
+    var centerMask = s.paper.circle(radial.cx, radial.cy, radial.r / 2.2).attr({ fill: "#fff"})
 
     // add in image of car in center (svg path)
   }
@@ -34,12 +34,13 @@ window.onload = function(){
   var gDark = s.paper.gradient("r(0,1,1)#000-#fff")
 
   var extendedCenterLines = function(){
-    var centerLeftLine = s.rect(500, 400, 2, 425).attr({transform:"rotate(90 501 400)", fill: g, stroke: g})
-    var centerRightLine= s.rect(500, 400, 2, 425).attr({transform:"rotate(270 501 400)", fill: gDark, stroke: gDark})
-    s.paper.circle(75, 400, 4).attr({fill: "#cf5300"})
-    s.paper.circle(925, 400, 4).attr({fill: "#000"})
+    var extension = 70
+    var centerLeftLine = s.rect(radial.cx, radial.cy, 2, radial.r + extension).attr({transform:"rotate(90 " + (radial.cx + 1) + " " + radial.cy +")", fill: g, stroke: g})
+    var centerRightLine= s.rect(radial.cx, radial.cy, 2, radial.r + extension).attr({transform:"rotate(270 " + (radial.cx + 1) + " " + radial.cy +")", fill: gDark, stroke: gDark})
+    s.paper.circle(radial.cx - radial.r - extension, radial.cy, 4).attr({fill: "#cf5300"})
+    s.paper.circle(radial.cx + radial.r + extension, radial.cy, 4).attr({fill: "#000"})
 
-    // create a group for setting opacity to 0, then call fadeInDelay(group, 500)
+    // create a group for setting opacity to 0, then call fadeInDelay(group, radial.cx)
   }
 
   // VERTICAL LINE CREATION
@@ -70,9 +71,22 @@ window.onload = function(){
     line.animate({opacity:1}, 600, mina.easeout)
   }
 
-  var radialText = function(){
-    // var
+  // HELPER TEXT FUNCTIONS
 
+  // leftText is {'title', 'description'}
+  var renderRadialText = function(text){
+    // refactor so position values aren't hard coded
+
+    var leftTitle = s.paper.text(250, 450, text.leftTitle).attr({"text-anchor": "end"})
+    var leftDescription = s.paper.text(250, 480, text.leftDescription).attr({"text-anchor": "end"})
+    var rightTitle = s.paper.text(700, 450, text.rightTitle).attr({"text-anchor": "start"})
+    var rightDescription = s.paper.text(700, 480, text.rightDescription).attr({"text-anchor": "start"})
+
+    var titles = s.group(leftTitle, rightTitle)
+    var descriptions = s.group(leftDescription, rightDescription)
+
+    titles.attr({fontSize: '1.8em'})
+    descriptions.attr({fontSize: '.7em'})
   }
 
   // ROUTER FUNCTION
@@ -90,20 +104,37 @@ window.onload = function(){
     } else if(style==="halfRadial"){
       radialFlare("half", lines)
       extendedCenterLines()
-      radialText()
+      renderRadialText(text)
     } else if(style==="fullRadial"){
       radialFlare("full", lines) // may need to make these calls more flexible, like the lines
       extendedCenterLines()
+      renderRadialText(text)
     } else if(style==="halfRadialMobile"){
       radialFlare("half", lines)
+      renderRadialText(text)
     }
+  }
+
+  // SAMPLE TEXT
+  var radialText = {
+    leftTitle: 'Sun Panels',
+    leftDescription: 'Panels harness power throughout the day',
+    rightTitle: 'Battery',
+    rightDescription: 'The battery stores energy for later use'
+  }
+
+  // refactor this to be passed in the creation function
+  var radial = {
+    cx: 500,
+    cy: 400,
+    r: 350
   }
 
    // UNCOMMENT TO SEE DIFFERENT STYLES
    // draw("horizontal", 50)
    // draw("vertical", 20)
-   // draw("halfRadial", 25)
-   // draw("fullRadial", 50)
-   draw("halfRadialMobile", 20)
+   draw("halfRadial", 25, radialText)
+   // draw("fullRadial", 50, radialText)
+   // draw("halfRadialMobile", 20, radialText)
 
 }
